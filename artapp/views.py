338bff2df714351a9849026e57a02fd141c8f03b
huyz <dummer@yeah.net>
2018-07-03 17:18:23 +0800

    '修改主页面'

--- artapp/views.py
+++ artapp/views.py
@@ -1,20 +1,16 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from artapp.models import ArtTag
+from artapp.models import ArtTag,Art
 
 def index(request):
-    # 请求路径和请求方法
-    print(request.path, request.method)
-    # 请求头的元信息和GET请求参数(查询参数)
-    # print(request.META, request.GET)
-    print(request.GET)
-    print(request.GET.get('page'), request.GET.get('tag'))
-    # POST请求参数(表单参数)
-    print(request.POST)
-    # return HttpResponse('<h1>您好</h1>')
-    # return JsonResponse({'name': 'huyz', 'age': 20})
-    return render(request, 'art/list.html', context={'name': 'huyz', 'age': 20})
+
+
+
+    # 返回渲染模板
+    return render(request, 'art/list.html',
+                  context={'art':Art.objects.all(),
+                           'tags': ArtTag.objects.all()})
 
 
 def add_tags(request):
